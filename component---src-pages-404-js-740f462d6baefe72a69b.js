4f5e942b9a34e0f0afb974cf76655d31c60138e7
openmymai <maicmi@me.com>
2022-11-16 22:54:23 +0700

    Updates

---
+++ component---src-pages-404-js-740f462d6baefe72a69b.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkprotandim=self.webpackChunkprotandim||[]).push([[883],{429:function(e,t,n){n.r(t);var l=n(7294),u=n(3069),a=n(8183);t.default=()=>l.createElement(u.Z,null,l.createElement(a.Z,{title:"404: Not found"}),l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}]);
+//# sourceMappingURL=component---src-pages-404-js-740f462d6baefe72a69b.js.map
